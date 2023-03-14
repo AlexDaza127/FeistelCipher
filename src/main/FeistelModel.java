f0ff51f524e0d8dce22025abf2556bbe46efcce4
AlexDaza127 <michaeldaza90@gmail.com>
2023-03-14 00:58:33 -0500

    Se obtiene el diccinario de ascci en dec a bin y se obtiene el bin de los hex del mensaje

--- src/main/FeistelModel.java
+++ src/main/FeistelModel.java
@@ -1,5 +1,47 @@
 package main;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FeistelModel {
+
+	public void initializer(String msg) {
+		
+		List<String> msgBin = new ArrayList<>();
+		msgBin.addAll(asciiToBinMsg(msg));
+
+		String[] msgSplit = msg.split(",");
+		for (int i = 0; i < msgSplit.length; i++) {
+			System.out.println(msgSplit[i] + " = " + msgBin.get(i));
+		}
+		
+		System.out.println("\nascii\n");
+		for (int index : ascciDecToBin().keySet()) {
+			System.out.println(index + " = " + ascciDecToBin().get(index));
+		}
+		
+	}
+
+	public List<String> asciiToBinMsg(String msg) {
+		List<String> msgBin = new ArrayList<>();
+		String[] msgSplit = msg.split(",");
+		for (String m : msgSplit) {
+			msgBin.add(String.format("%08d",Integer.parseInt(new BigInteger(m, 16).toString(2))));
+		}
+		return msgBin;
+	}
+	
+	public Map<Integer, String> ascciDecToBin(){
+		Map<Integer, String> ascciConverter = new HashMap<>();
+		
+		for (int i = 32; i < 256; i++) {
+			ascciConverter.put(i,String.format("%08d",Integer.parseInt(new BigInteger(String.valueOf(i), 10).toString(2))));
+		}
+		
+		return ascciConverter;
+	}
 
 }
--- src/main/MainCipher.java
+++ src/main/MainCipher.java
@@ -4,7 +4,9 @@ public class MainCipher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String msg = "ab,49,72,7e,22,64,6f,63,68,22,77,69,75,22,62,63,74,65,6f,60,75,67,75,23";
+		FeistelModel feistelModel = new FeistelModel();
+		feistelModel.initializer(msg);
 	}
 
 }
